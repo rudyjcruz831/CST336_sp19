ebda4543ee8bdca8af02c83c7af59bf231609b2e
Rudy Cruz <rudcruz@csumb.edu>
2019-05-12 06:58:09 +0000

    Second state of Timerly

---
+++ Timer/index.js
@@ -0,0 +1,18 @@
+$(document).on('click', '.updateLink', function() {
+    console.log("Update link clicked");
+    $("#manageTime").modal("show");
+    // console.log($("name[appt1]").val());
+    $("#timeEnterbutton").on('click' , function(){
+            console.log("Start Time: " + $('#appt1').val());    
+            console.log("End Time: "+  $('#appt2').val());
+            console.log("Date: " + $('#appt0').val());
+    })// end of timeEnterButton
+    $("#logOutButton").on('click', function(){
+        console.log("Log out Butten press");
+    });// end of logOutBotton
+});
+$(document).ready(function() {$
+    $("#logOutButton").on('click', function(){
+        console.log("Log out Butten press");
+    });// end of logOutBotton
+}); // end of docuemtn ready
---
+++ Timer/logIn/logIn.js
@@ -0,0 +1,16 @@
+$(document).on('click', '.updateLink', function() {
+    console.log("Update link clicked");
+    $("#manageTime").modal("show");
+    // console.log($("name[appt1]").val());
+    // $("#timeEnterbutton").on('click' , function(){
+    //         console.log("Start Time: " + $('#appt1').val());    
+    //         console.log("End Time: "+  $('#appt2').val());
+    //         console.log("Date: " + $('#appt0').val());
+    // })// end of timeEnterButton
+    // $("#logOutButton").on('click', function(){
+        // console.log("Log out Butten press");
+    // });// end of logOutBotton
+});
+$(document).ready(function(){
+    
+}); // end of document ready 
